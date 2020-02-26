0522410fd66e3e5038d308db788781b6289df48d
Hermitter <chacin.carlos9798@gmail.com>
2020-02-26 14:46:51 -0500

    add gpio value reading

--- src/gpio/mod.rs
+++ src/gpio/mod.rs
@@ -1,3 +1,5 @@
+use crate::bus::memory_map::*;
+use crate::error::Error;
 use crate::Bus;
 
 /// Controls the GPIO pins on a MATRIX device.
@@ -6,19 +8,52 @@ pub struct Gpio<'a> {
 }
 
 impl<'a> Gpio<'a> {
+    /// Returns an instance of GPIO.
     pub fn new(bus: &'a Bus) -> Gpio {
         Gpio { bus }
     }
 
-    pub fn get_value(self, pin: u8) -> u8 {
+    /// Returns the digital value of a MATRIX GPIO pin (0-15).
+    pub fn get_value(&self, pin: u8) -> u8 {
+        if pin > 15 {
+            panic!("The MATRIX Voice/Creator GPIO pins are from 0-15");
+        }
+
+        // create read buffer
+        let mut data: [u32; 3] = [0; 3];
+
+        data[0] = (fpga_address::GPIO + 1) as u32;
+        data[1] = 2; // 2 bytes needed (8*2 = 16 pins)
+
+        // populate buffer
+        // the buffer will be passed a value that contains the state of each GPIO pin
+        self.bus
+            .read(unsafe { std::mem::transmute::<&mut [u32], &mut [u8]>(&mut data) });
+
+        // bit operation to extract the current pin state
         let mask = 0x1 << pin;
+        ((data[2] & mask) >> pin) as u8
+    }
 
-        // uint16_t value;
-        // bus_->Read(kGPIOBaseAddress + 1, &value);
-        // value = (value & mask) >> pin;
+    pub fn get_values(&self) -> [u8; 16] {
+        // create read buffer
+        let mut data: [u32; 3] = [0; 3];
 
-        // return value;
+        data[0] = (fpga_address::GPIO + 1) as u32;
+        data[1] = 2; // 2 bytes needed (8*2 = 16 pins)
 
-        0
+        // populate buffer
+        // the buffer will be passed a value that contains the state of each GPIO pin
+        self.bus
+            .read(unsafe { std::mem::transmute::<&mut [u32], &mut [u8]>(&mut data) });
+
+        // bit operation to extract each pin state (0-15)
+        let mut pins = [0; 16];
+        for i in 0..16 {
+            let mask = 0x1 << i;
+            pins[i] = ((data[2] & mask) >> i) as u8
+        }
+
+        pins
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,38 @@ fn main() {
     let bus = hal::Bus::init().unwrap();
     let sensors = hal::Sensors::new(&bus);
     let everloop = hal::Everloop::new(&bus);
+    let gpio = hal::Gpio::new(&bus);
 
     everloop.set_all(hal::Rgbw::new(0, 0, 0, 0));
 
-    // loop {
-    test_sensors(&sensors);
-    delay(100);
-    // }
+    loop {
+        println!("{:?}", gpio.get_values());
+        // test_gpio_get_value();
+        // test_sensors(&sensors);
+        // delay(1);
+    }
+}
+
+fn test_gpio_get_value(gpio: &hal::Gpio) {
+    println!(
+        "{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}",
+        gpio.get_value(0),
+        gpio.get_value(1),
+        gpio.get_value(2),
+        gpio.get_value(3),
+        gpio.get_value(4),
+        gpio.get_value(5),
+        gpio.get_value(6),
+        gpio.get_value(7),
+        gpio.get_value(8),
+        gpio.get_value(9),
+        gpio.get_value(10),
+        gpio.get_value(11),
+        gpio.get_value(12),
+        gpio.get_value(13),
+        gpio.get_value(14),
+        gpio.get_value(15),
+    );
 }
 
 fn test_sensors(sensors: &hal::Sensors) {
