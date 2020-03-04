d9e19a3d8504f307ffb88cbf8d372c33e908ed46
Hermitter <chacin.carlos9798@gmail.com>
2020-03-04 18:20:21 -0500

    minor fixes to gpio bus write

--- src/gpio/set.rs
+++ src/gpio/set.rs
@@ -2,6 +2,7 @@ use super::config::*;
 use super::Gpio;
 use crate::bus::memory_map::*;
 use crate::error::Error;
+use std::mem;
 
 impl<'a> Gpio<'a> {
     /// Configure a specific pin's mode, function, state, etc..
@@ -36,12 +37,12 @@ impl<'a> Gpio<'a> {
     }
 
     /// Shortener to set pin configurations. `value` & `address_offset` are directly passed into the bus' write buffer.
-    fn bus_write(&self, value: u32, address_offset: u16) {
+    fn bus_write(&self, value: u16, address_offset: u16) {
         // create and populate write buffer
         let mut buffer: [u32; 3] = [0; 3];
         buffer[0] = (fpga_address::GPIO + address_offset) as u32; // address to write to
-        buffer[1] = 2; // byte length of value
-        buffer[2] = value;
+        buffer[1] = mem::size_of_val(&value) as u32; // byte length of value
+        buffer[2] = value as u32;
 
         self.bus
             .write(unsafe { std::mem::transmute::<&mut [u32], &mut [u8]>(&mut buffer) });
@@ -53,7 +54,7 @@ impl<'a> Gpio<'a> {
         let mut prescaler = self.prescaler_bank_map.lock()?;
         *prescaler = *prescaler << (4 * bank) | (*prescaler & !mask);
 
-        self.bus_write((fpga_address::GPIO + 3) as u32, *prescaler);
+        self.bus_write(*prescaler, fpga_address::GPIO + 3);
         Ok(())
     }
 }
