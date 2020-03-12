950a68aff6405fc87c59a5968c8b5923faffcbf5
Hermitter <chacin.carlos9798@gmail.com>
2020-03-12 16:10:47 -0400

    test time

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,22 @@
 // This file is just meant to test things out.
 // use hal::gpio::config::*;
 use matrix_rhal as hal;
+use std::time::{Duration, Instant};
 use std::{thread, time};
 
 fn main() {
+    let now = Instant::now();
     let bus = hal::bus::init().unwrap();
+    println!("Bus Created: {:#?}", now.elapsed());
 
-    println!("{:?}", bus);
     let sensors = hal::Sensors::new(&bus);
-    let everloop = hal::Everloop::new(&bus);
-    let gpio = hal::Gpio::new(&bus);
 
-    everloop.set_all(hal::Rgbw::black());
-
-    // test_gpio_set_value(&gpio);
-    // test_gpio_pwm(&gpio);
-    // test_gpio_set_servo(&gpio);
-
-    test_sensors(&sensors);
-
-    loop {
+    for _ in 0..1000 {
         // println!("{:?}", gpio.get_states());
         test_sensors(&sensors);
         // delay(100);
     }
+    println!("Sensor Reads: {:#?}", now.elapsed());
 }
 
 // fn test_gpio_set_value(gpio: &hal::Gpio) {
@@ -50,10 +43,15 @@ fn main() {
 // }
 
 fn test_sensors(sensors: &hal::Sensors) {
-    println!("--> {:#?}", sensors.read_uv());
-    println!("--> {:#?}", sensors.read_pressure());
-    println!("--> {:#?}", sensors.read_humidity());
-    println!("--> {:#?}", sensors.read_imu());
+    // println!("--> {:#?}", sensors.read_uv());
+    // println!("--> {:#?}", sensors.read_pressure());
+    // println!("--> {:#?}", sensors.read_humidity());
+    // println!("--> {:#?}", sensors.read_imu());
+
+    sensors.read_uv();
+    sensors.read_pressure();
+    sensors.read_humidity();
+    sensors.read_imu();
 }
 
 fn delay(ms: u64) {
